1b5999d8d66ca55d7359b073befe2d0517bcb1a5
Marcos Huang <marcos.kai.huang@gmail.com>
2021-05-16 01:07:53 -0300

    Implementing possible moves of Rook Method ChessPiece.IsThereOpponentPiece(position) [protected] Implement Rook.PossibleMoves Method ChessMatch.ValidateTargetPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -41,10 +41,17 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
     
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+    }
+        
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 /**
  *
  * @author MARCOSHUANG
@@ -21,5 +18,10 @@ public abstract class ChessPiece extends Piece{
 
     public Color getColor() {
         return color;
+    }
+    
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 }
