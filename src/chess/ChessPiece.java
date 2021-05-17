2fe733e1c573322b41babd194c6a9e0f472ad383
Marcos Huang <marcos.kai.huang@gmail.com>
2021-05-17 12:20:09 -0300

    Piece move count Class ChessPiece: Class ChessPiece, Method IncreaseMoveCount, Method DecreaseMoveCount Class ChessMatch: Update MakeMove, Update UndoMove

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -10,7 +10,20 @@ import boardgame.Position;
  */
 public abstract class ChessPiece extends Piece{
     private Color color;
+    private int moveCount;
+    
+    public int getMoveCount() {
+        return moveCount;
+    }
 
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+    
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
